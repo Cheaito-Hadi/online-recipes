1260d01f76aefffc3226ff3b44d0c40862ae6f68
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-21 23:45:48 +0300

    Client <> Add to shoppingList

--- client/src/components/RecipeCard/index.jsx
+++ client/src/components/RecipeCard/index.jsx
@@ -1,40 +1,59 @@
-import React, {useState} from 'react';
-import { AiFillHeart } from 'react-icons/ai';
-import { BiSolidCommentMinus } from 'react-icons/bi';
-import { IoMdShareAlt } from 'react-icons/io';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import './styles.css'
-import CommentModal from '../../components/CommentModal'
+import React, { useState } from "react";
+import { AiFillHeart } from "react-icons/ai";
+import { BiSolidCommentMinus } from "react-icons/bi";
+import { IoMdShareAlt } from "react-icons/io";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./styles.css";
+import CommentModal from "../../components/CommentModal";
+import axios from "axios";
 
 function RecipeCard({ recipe }) {
   const [isCommentModalOpen, setIsCommentModalOpen] = useState(false);
-    const openModal = () => {
-        setIsCommentModalOpen(true);
-    };
+  const openModal = () => {
+    setIsCommentModalOpen(true);
+  };
 
-    const closeModal = () => {
-        setIsCommentModalOpen(false);
-    };
-  const url = 'http://127.0.0.1:8000/api';
-   
+  const closeModal = () => {
+    setIsCommentModalOpen(false);
+  };
+  const url = "http://127.0.0.1:8000/api";
+
+  const handleAddToShoppingList = async () => {
+    try {
+      await axios.post(
+        `${url}/create_shoppinglist`,
+        { recipe_id: recipe.id },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.error("Error adding to list:", error);
+    }
+  };
+
   return (
     <div className="card-container">
       <div className="title-container">
         <h3 className="title">{recipe.name}</h3>
         <div className="icons-container">
           <AiFillHeart /> {recipe.likes_count}
-          <a onClick={openModal}><BiSolidCommentMinus /> {recipe.comments_count}</a>
+          <a onClick={openModal}>
+            <BiSolidCommentMinus /> {recipe.comments_count}
+          </a>
           <IoMdShareAlt />
         </div>
       </div>
       {isCommentModalOpen && (
-              <CommentModal closeModal={closeModal} recipeId={recipe.id}/>
-          )}
+        <CommentModal closeModal={closeModal} recipeId={recipe.id} />
+      )}
       <Carousel className="images-container">
         {recipe.images.map((image) => (
           <div key={image.id}>
-            <img src={url + '/get_image/' + image.id} alt="Imag" />
+            <img src={url + "/get_image/" + image.id} alt="Imag" />
           </div>
         ))}
       </Carousel>
@@ -43,12 +62,16 @@ function RecipeCard({ recipe }) {
         <h4>Ingredients:</h4>
         <ul>
           {recipe.ingredients.map((ingredient) => (
-            <li key={ingredient.id}>{ingredient.name} /Amount: {ingredient.pivot.amount}Kg</li>
+            <li key={ingredient.id}>
+              {ingredient.name} /Amount: {ingredient.pivot.amount}Kg
+            </li>
           ))}
         </ul>
       </div>
       <div className="button-container">
-        <button className="list-btn">Add to List</button>
+        <button className="list-btn" onClick={handleAddToShoppingList}>
+          Add to List
+        </button>
       </div>
     </div>
   );

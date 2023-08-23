21f85eee91887016b9cb8152aabb5155a04c7107
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-23 18:24:50 +0300

    Client <> Landing Page Styling

--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -1,47 +1,47 @@
-    import React, {useEffect, useState} from 'react';
-    import './styles.css';
-    import RecipeCard from '../../components/RecipeCard';
-    import axios from 'axios';
-    import RecipeModal from "../../components/RecipeModal";
+import React, {useEffect, useState} from 'react';
+import './styles.css';
+import RecipeCard from '../../components/RecipeCard';
+import axios from 'axios';
+import RecipeModal from "../../components/RecipeModal";
 
-    function Landing() {
-        const url = 'http://127.0.0.1:8000/api';
+function Landing() {
+    const url = 'http://127.0.0.1:8000/api';
 
-        const [recipeData, setRecipeData] = useState([]);
-        const [originalRecipeData, setOriginalRecipeData] = useState([]);
-        const [isModalOpen, setIsModalOpen] = useState(false);
-        const [searchName, setSearchName] = useState();
-        const openModal = () => {
-            setIsModalOpen(true);
-        };
+    const [recipeData, setRecipeData] = useState([]);
+    const [originalRecipeData, setOriginalRecipeData] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [searchName, setSearchName] = useState();
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
 
-        const closeModal = () => {
-            setIsModalOpen(false);
-        };
-        useEffect(() => {
-            async function fetchRecipeData() {
-                try {
-                    const response = await axios.get(url + '/get_recipes', {
-                        headers: {
-                            Authorization: `Bearer ${localStorage.getItem("token")}`,
-                        },
-                    });
-                    if (response.data.message === 'success') {
-                        setRecipeData(response.data.recipe_data);
-                        setOriginalRecipeData(response.data.recipe_data);
-                    }
-                } catch (error) {
-                    console.error('Error fetching recipe data:', error);
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+    useEffect(() => {
+        async function fetchRecipeData() {
+            try {
+                const response = await axios.get(url + '/get_recipes', {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },
+                });
+                if (response.data.message === 'success') {
+                    setRecipeData(response.data.recipe_data);
+                    setOriginalRecipeData(response.data.recipe_data);
                 }
+            } catch (error) {
+                console.error('Error fetching recipe data:', error);
             }
+        }
 
-            fetchRecipeData();
-        }, []);
-        
-        const filterBySearch = (event) => {
-            // debugger;
-            const keyword = event.target.value.toLowerCase();
-            if (keyword !== '') {
+        fetchRecipeData();
+    }, []);
+
+    const filterBySearch = (event) => {
+        // debugger;
+        const keyword = event.target.value.toLowerCase();
+        if (keyword !== '') {
 
             const filteredResults = recipeData.filter((recipe) => {
                 const nameMatch = (
@@ -56,26 +56,27 @@
             });
             //   debugger;
             setRecipeData(filteredResults);
-                } else {
-                    setRecipeData(originalRecipeData);
-                }
-            setSearchName(keyword)
-        };
-        return (
-            <div className="landing-container">
-                <input id="search-box" value={searchName} onChange={filterBySearch} placeholder="Search by name, cuisine, or ingredient..."/>
-                <div className="modal-create-main">
-                    <button onClick={openModal}>Create a Recipe</button>
-                </div>
-                {isModalOpen && (
-                    <RecipeModal closeModal={closeModal}/>
-                )}
-                <div className="recipe-container">
+        } else {
+            setRecipeData(originalRecipeData);
+        }
+        setSearchName(keyword)
+    };
+    return (
+        <div className="landing-container">
+            <div className="search-create-wrapper">
+                <input className="search-bar" id="search-box" value={searchName} onChange={filterBySearch}
+                       placeholder="Search by name, cuisine, or ingredient..."/>
+                <button className="create-recipe-btn" onClick={openModal}>Create a Recipe</button>
+            </div>
+            {isModalOpen && (
+                <RecipeModal closeModal={closeModal}/>
+            )}
+            <div className="recipe-container">
                 {recipeData.map((recipe) => (
                     <RecipeCard key={recipe.id} recipe={recipe}/>
                 ))}</div>
-            </div>
-        );
-    }
+        </div>
+    );
+}
 
-    export default Landing;
+export default Landing;

5eb827b2f7948b2a3de412090b0a78e2b5fbafaa
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-08-23 14:40:58 +0300

    Client <> Shopping List Styling

--- client/src/pages/ShoppingList/index.jsx
+++ client/src/pages/ShoppingList/index.jsx
@@ -1,33 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './styles.css';
 import ShoppingListCard from '../../components/ShoppingListCard';
 
 function ShoppingList() {
-  const [recipes, setRecipes] = useState([]);
+    const [recipes, setRecipes] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/get_shoppinglist', {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`
-      }
-    })
-      .then(response => {
-        const recipeData = response.data.recipe_data;
-        setRecipes(recipeData);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/get_shoppinglist', {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+            .then(response => {
+                const recipeData = response.data.recipe_data;
+                setRecipes(recipeData);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
 
-  return (
-    <div>
-      {recipes.map((recipe, index) => (
-        <ShoppingListCard key={index} item={recipe} />
-      ))}
-    </div>
-  );
+    return (
+        <>
+            <h2 className="page-headings">My Shopping Lists: </h2>
+            <div className="shopping-container">
+                {recipes.map((recipe, index) => (
+                    <ShoppingListCard key={index} item={recipe}/>
+                ))}
+            </div>
+        </>
+    );
 }
 
 export default ShoppingList;
